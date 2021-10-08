30bd84ccaf510de2337b6145e010bac6c9781488
DESKTOP-3KUJ1QG\Dandar <darkdandar@yahoo.it>
2021-10-08 17:40:32 +0200

    Added MinesweeperGameUI module

---
+++ Source/MinesweeperGameUI/MinesweeperGameUI.Build.cs
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MinesweeperGameUI : ModuleRules
+{
+	public MinesweeperGameUI(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+				
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"Engine"
+			}
+			);
+	}
+}
