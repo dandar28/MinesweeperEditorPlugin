0fedbd871f05e9018ea53440aa58ac560e2af5ee
DESKTOP-3KUJ1QG\Dandar <darkdandar@yahoo.it>
2021-10-10 00:29:07 +0200

    Removed copyright notice from source

--- Source/MinesweeperGameLogics/Private/MinesweeperGameLogicsModule.cpp
+++ Source/MinesweeperGameLogics/Private/MinesweeperGameLogicsModule.cpp
@@ -1,5 +1,3 @@
-// Copyright (c) 2014-2021 Zuru Tech HK Limited, All rights reserved.
-
 #include "MinesweeperGameLogicsModule.h"
 
 IMPLEMENT_MODULE(FMinesweeperGameLogicsModule, MinesweeperGameLogics);
