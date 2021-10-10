d8751c7abb27f8232cf4fe6d131cd5e0d5dec11e
DESKTOP-3KUJ1QG\Dandar <darkdandar@yahoo.it>
2021-10-10 21:06:33 +0200

    Added selectable difficulties among combo options and also their interaction with the settings to be editable only when in custom mode

--- Source/MinesweeperGameUI/Public/SMinesweeperGameBoard.h
+++ Source/MinesweeperGameUI/Public/SMinesweeperGameBoard.h
@@ -10,6 +10,16 @@
 #include "TNumericSettingEntry.h"
 
 /**
+ * \brief - Enum containing the available difficulty levels settable for the game.
+ */
+enum EDifficultyLevel {
+	Beginner,
+	Intermediate,
+	Expert,
+	Custom
+};
+
+/**
  * \brief - Slate widget that renders a minesweeper game board visually and handles the
  *			game logic bindings with the UI updates through an instanced game session.
  */
@@ -90,6 +100,12 @@ private:
 	TSharedRef<SVerticalBox> _makeMainGameArea();
 
 	/**
+	 * \brief - Update the difficulty level with a new one.
+	 * \param[in] InNewDifficultyLevel - New difficulty level to be set.
+	 */
+	void _setNewDifficultyLevel(EDifficultyLevel InNewDifficultyLevel);
+
+	/**
 	 * \brief - Owned game session that is running on this game board UI widget.
 	 */
 	TSharedPtr<FMinesweeperGameSession> _gameSession;
@@ -125,6 +141,11 @@ private:
 	};
 
 	/**
+	 * \brief - Currently selected predefined difficulty level index.
+	 */
+	int32 _selectedDifficultyIndex = 0;
+
+	/**
 	 * \brief - Currently selected action index.
 	 */
 	int32 _selectedActionIndex = 0;
@@ -145,18 +166,23 @@ private:
 	TSharedPtr<SNumericEntryBox<int>> _numericEntryNumberOfMines;
 
 	/**
+	 * \brief - Slate object of the difficulty combo box setting.
+	 */
+	TSharedPtr<SComboBox<TSharedPtr<FText>>> _difficultyComboBox;
+
+	/**
 	 * \brief - Shared pointer to the actual instance of the game settings updated from the UI.
 	 */
 	TSharedPtr<FMinesweeperGameSettings> _gameSettings;
-	
+
 	/**
-	 * \brief - Enum containing the available difficulty levels settable for the game.
+	 * \brief - Array of options for the possible difficulties to be chosen.
 	 */
-	enum EDifficultyLevel {
-		Beginner,
-		Intermediate,
-		Expert,
-		Custom
+	const TArray<TSharedPtr<FText>> _difficultyComboOptions = TArray<TSharedPtr<FText>>{
+		MakeShared<FText>(FText::FromString("Beginner")),
+		MakeShared<FText>(FText::FromString("Intermediate")),
+		MakeShared<FText>(FText::FromString("Expert")),
+		MakeShared<FText>(FText::FromString("Custom"))
 	};
 
 	/**
