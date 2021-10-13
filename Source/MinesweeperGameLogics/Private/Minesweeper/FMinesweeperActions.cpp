2bf069ae877300adb08ae198fbb22bd96844d3c0
Dandar <darkdandar@yahoo.it>
2021-10-13 17:05:06 +0200

    Added FMinesweeperActions

---
+++ Source/MinesweeperGameLogics/Private/Minesweeper/FMinesweeperActions.cpp
@@ -0,0 +1,80 @@
+#include "Minesweeper/FMinesweeperActions.h"
+
+#include "CellMatrix/ICellMatrix.h"
+#include "CellMatrix/TCellMatrix.h"
+
+#include "Minesweeper/FMinesweeperCellCoordinate.h"
+#include "Minesweeper/FMinesweeperCell.h"
+#include "Minesweeper/FMinesweeperMatrixNavigator.h"
+#include "Minesweeper/FMinesweeperGameDataState.h"
+#include "Minesweeper/FMinesweeperGameSession.h"
+
+#include "Minesweeper/LogicStates/FGameOverLogicState.h"
+#include "Minesweeper/LogicStates/FGameWinLogicState.h"
+
+class FMinesweeperActions::FMinesweeperActionSweep : public IMinesweeperAction {
+	void Perform(const TSharedRef<FMinesweeperGameSession> InGameSession, const FMinesweeperCellCoordinate& InCoordinates) override {
+		check(InGameSession->IsRunning());
+
+		auto LogicStateMachine = InGameSession->GetLogicStateMachine();
+
+		auto GameDataState = InGameSession->GetGameDataState();
+		check(GameDataState->Matrix->Has(InCoordinates));
+
+		FMinesweeperCell& Cell = GameDataState->Matrix->Get(InCoordinates);
+
+		if (Cell.IsRevealed()) {
+			// If the cell is already revealed, do nothing.
+			return;
+		}
+
+		// Reveal the target cell.
+		Cell.SetRevealed(true);
+
+		switch (Cell.CellState) {
+		case EMinesweeperCellState::Bomb:
+			// When interacting with a bomb, transit to game over.
+			LogicStateMachine->GoToState<FGameOverLogicState>();
+			break;
+		case EMinesweeperCellState::Empty:
+			// When interacting with an empty space, reveal all its adjacent empty cells recursively (until some bombs are reached in order to stop recursing).
+			auto MinesweeperMatrixNavigator = FMinesweeperMatrixNavigator(GameDataState->Matrix.ToSharedRef());
+			MinesweeperMatrixNavigator.RevealAdjacentEmptyCellsRecursively(InCoordinates);
+
+			// Win detection.
+			if (GameDataState->IsGameWon()) {
+				LogicStateMachine->GoToState<FGameWinLogicState>();
+			}
+		}
+	}
+};
+
+class FMinesweeperActions::FMinesweeperActionFlag : public IMinesweeperAction {
+	void Perform(const TSharedRef<FMinesweeperGameSession> InGameSession, const FMinesweeperCellCoordinate& InCoordinates) override {
+		check(InGameSession->IsRunning());
+
+		auto GameDataState = InGameSession->GetGameDataState();
+		check(GameDataState->Matrix->Has(InCoordinates));
+
+		// Toggle the flag on the target cell.
+		FMinesweeperCell& Cell = GameDataState->Matrix->Get(InCoordinates);
+		Cell.SetFlagged(!Cell.IsFlagged());
+	}
+};
+
+class FMinesweeperActions::FMinesweeperActionMark : public IMinesweeperAction {
+	void Perform(const TSharedRef<FMinesweeperGameSession> InGameSession, const FMinesweeperCellCoordinate& InCoordinates) override {
+		check(InGameSession->IsRunning());
+
+		auto GameDataState = InGameSession->GetGameDataState();
+		check(GameDataState->Matrix->Has(InCoordinates));
+
+		// Toggle the question mark on the target cell.
+		FMinesweeperCell& Cell = GameDataState->Matrix->Get(InCoordinates);
+		Cell.SetQuestionMarked(!Cell.IsQuestionMarked());
+	}
+};
+
+TSharedRef<IMinesweeperAction> FMinesweeperActions::Sweep = MakeShared<FMinesweeperActionSweep>();
+TSharedRef<IMinesweeperAction> FMinesweeperActions::Flag = MakeShared<FMinesweeperActionFlag>();
+TSharedRef<IMinesweeperAction> FMinesweeperActions::Mark = MakeShared<FMinesweeperActionMark>();
---
+++ Source/MinesweeperGameLogics/Public/Minesweeper/FMinesweeperActions.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "CellMatrix/ICellMatrix.h"
+
+#include "Minesweeper/FTickTimer.h"
+#include "Minesweeper/FMinesweeperCell.h"
+#include "Minesweeper/FMinesweeperCellCoordinate.h"
+
+struct IMinesweeperAction {
+	virtual void Perform(const TSharedRef<class FMinesweeperGameSession> InGameSession, const FMinesweeperCellCoordinate& InCoordinates) = 0;
+};
+
+struct FMinesweeperActions {
+	//<<< Publicly accessible implemented actions
+	static TSharedRef<IMinesweeperAction> Sweep;
+	static TSharedRef<IMinesweeperAction> Flag;
+	static TSharedRef<IMinesweeperAction> Mark;
+	//>>>
+
+	//<<< Private Implementations
+	class FMinesweeperActionSweep;
+	class FMinesweeperActionFlag;
+	class FMinesweeperActionMark;
+	//>>>
+};
+
+struct FRegisteredAction {
+	FDateTime Time;
+	TSharedRef<IMinesweeperAction> Action;
+	FMinesweeperCellCoordinate InteractedCell;
+};
+
+struct FActionHistory {
+	TArray<FRegisteredAction> Actions;
+};
