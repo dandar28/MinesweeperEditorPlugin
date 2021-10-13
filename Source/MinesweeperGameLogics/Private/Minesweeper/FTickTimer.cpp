1a908bccb9bbf5366685d114de8d676ac8ddcc60
Dandar <darkdandar@yahoo.it>
2021-10-13 14:06:50 +0200

    Added FTickTimer class to represent a timer to be started/stopped for calculating elapsed time of the game

---
+++ Source/MinesweeperGameLogics/Private/Minesweeper/FTickTimer.cpp
@@ -0,0 +1,41 @@
+#include "..\..\Public\Minesweeper\FTickTimer.h"
+#include "..\..\Public\Minesweeper\FTickTimer.h"
+#include "..\..\Public\Minesweeper\FTickTimer.h"
+#include "Minesweeper/FTickTimer.h"
+
+void FTickTimer::StartTimer() {
+	_timeStart = FDateTime::Now();
+	_timeEnd.Reset();
+}
+
+void FTickTimer::StopTimer() {
+	check(HasTimerBeenStarted());
+	_timeEnd = FDateTime::Now();
+}
+
+FDateTime FTickTimer::GetTimeStart() const {
+	check(HasTimerBeenStarted());
+	return _timeStart.GetValue();
+}
+
+FDateTime FTickTimer::GetTimeEnd() const {
+	check(HasTimerBeenStopped());
+	return _timeEnd.GetValue();
+}
+
+FTimespan FTickTimer::GetTimeElapsedFromStart() const {
+	check(HasTimerBeenStarted());
+	return _timeEnd.Get(FDateTime::Now()) - _timeStart.GetValue();
+}
+
+bool FTickTimer::IsTimerRunning() const {
+	return HasTimerBeenStarted() && !HasTimerBeenStopped();
+}
+
+bool FTickTimer::HasTimerBeenStarted() const {
+	return _timeStart.IsSet();
+}
+
+bool FTickTimer::HasTimerBeenStopped() const {
+	return _timeEnd.IsSet();
+}
