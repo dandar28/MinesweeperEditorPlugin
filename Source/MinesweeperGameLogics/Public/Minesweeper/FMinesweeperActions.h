eea565dfdd4178bc622df976b8431157654cb7a5
Dandar <darkdandar@yahoo.it>
2021-10-14 20:48:21 +0200

    Added few docstring comments;

--- Source/MinesweeperGameLogics/Public/Minesweeper/FMinesweeperActions.h
+++ Source/MinesweeperGameLogics/Public/Minesweeper/FMinesweeperActions.h
@@ -8,10 +8,16 @@
 #include "Minesweeper/FMinesweeperCell.h"
 #include "Minesweeper/FMinesweeperCellCoordinate.h"
 
+/**
+ * \brief - Interface for generic minesweeper actions that can be performed with respect to a game session on a certain cell coordinate.
+ */
 struct IMinesweeperAction {
 	virtual void Perform(const TSharedRef<class FMinesweeperGameSession> InGameSession, const FMinesweeperCellCoordinate& InCoordinates) = 0;
 };
 
+/**
+ * \brief - Static accessor for action implementations.
+ */
 struct FMinesweeperActions {
 	//<<< Publicly accessible implemented actions
 	static TSharedRef<IMinesweeperAction> Sweep;
@@ -26,12 +32,18 @@ struct FMinesweeperActions {
 	//>>>
 };
 
+/**
+ * \brief - Structure for an action that has been registered, in order to be able to add it to an history.
+ */
 struct FRegisteredAction {
 	FDateTime Time;
 	TSharedPtr<IMinesweeperAction> Action;
 	FMinesweeperCellCoordinate InteractedCell;
 };
 
+/**
+ * \brief - An history of registered actions.
+ */
 struct FActionHistory {
 	TArray<FRegisteredAction> Actions;
 };
